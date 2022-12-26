6697f90465ffd54b54380f0105231d3e70a448c7
Elvin Jamalzade <camalzadeelvin@itstep.edu.az>
2022-12-26 12:56:34 +0400

    Add project files.

---
+++ ProxyApp/Services/FileHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ProxyApp.Services
+{
+    /// <summary>
+    /// This class help us to read data from specific file
+    /// </summary>
+    public class FileHelper
+    {
+        private static string FileName = @"../../Files\Words.txt";
+        /// <summary>
+        ///  This function get data from Specific file 
+        ///  if data not found returns null
+        /// </summary>
+        /// <returns></returns>
+        public static string[] GetAllData()
+        {
+            if (File.Exists(FileName))
+            {
+                var text = File.ReadAllText(FileName);
+                var data = text.Split('\n');
+                return data;
+            }
+            return null;
+        }
+    }
+}
